a6b488c1bd942f427b10d4d5246eb06e3158a5cd
Drake Wright <rdrakewright@gmail.com>
2023-05-17 08:49:59 -0500

    Initial Commit

---
+++ Rock And Roll Trivia/Rock_And_Roll_TriviaApp.swift
@@ -0,0 +1,17 @@
+//
+//  Rock_And_Roll_TriviaApp.swift
+//  Rock And Roll Trivia
+//
+//  Created by Drake Wright on 5/17/23.
+//
+
+import SwiftUI
+
+@main
+struct Rock_And_Roll_TriviaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
